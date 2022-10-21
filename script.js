fcf42d1b75359d30d4048153e2c63c616da3be44
Hannah <104508879+Dreadheadhannah@users.noreply.github.com>
2022-10-21 20:48:01 +0000

    Added comments to make the code  easily readable.

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
+//points 
 let points = 0
+//range
 let Range = 2
+//username
 const username = prompt("What is your username?");
 
 function game(range = Range){
+    //generate random number
     const randNum = Math.floor(Math.random() * range) + 1
-    const guess = prompt(`Hello, ${username}, 
-    Can you guess the right generated number?`)
+    //prompt user to generate the number
+    const guess = prompt(`Hello ${username}, can you predict the generated number?`)
+    //check if user cancelled the game
     if(!guess || !username){
+        //message
         alert(`You ended the game with ${points} points.`);
+        //reset points
         points=0;
+        //reset range
         Range=2;
+        //stop the recursion
         return false;
     }
+    //check if user prediction is correct
     if( Number(guess) === randNum ){
+        //award a point to the user
         points+=1;
+        //increase range limit value
         Range+=1;
-        alert(`Your guess is correct,
-         you have ${points} points`);
+        alert(`Your guess is correct, you have ${points} points`);
     }else if( Number(guess) !== randNum ){
-        alert(`Your guess is incorrect, 
-        you have ${points} points. Try again?`);
+        alert(`Your guess is incorrect, you have ${points} points. Try again?`);
     }
+    //call function again
     game()
 }
 game()
